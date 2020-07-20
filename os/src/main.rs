ef1bcb5f3081c74b6e72f1e6e1326f8ebedaa31f
lolshoc <lolshoc@gmail.com>
2020-07-20 13:10:34 -0800

    now will load the kernel through qemu when you run the project
    prints Hello World!

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+extern crate rlibc;
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vgaBuffer = 0xb8000 as *mut u8;
+
+    for(i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vgaBuffer.offset(i as isize * 2) = byte;
+            *vgaBuffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop{}
 }
