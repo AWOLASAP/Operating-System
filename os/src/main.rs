61699146a547f72fa237346a905ac42268457b4b
lolshoc <lolshoc@gmail.com>
2020-07-22 10:56:10 -0800

    Added paging and heap allocation

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,8 +4,15 @@
 #![test_runner(os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
 use core::panic::PanicInfo;
 use os::println;
+use os::memory::{self, BootInfoFrameAllocator};
+use os::allocator;
+use bootloader::{BootInfo, entry_point};
+use x86_64::{VirtAddr, structures::paging::MapperAllSizes, structures::paging::Page};
+use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -20,11 +27,36 @@ fn panic(info: &PanicInfo) -> ! {
     os::test_panic_handler(info)
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+
+entry_point!(kernel_main);
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World!");
 
     os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     #[cfg(test)]
     test_main();
