48d207f557a63501ac98d2b8f3aed4bd7c57b629
Otis Root <otis@otisroot.com>
2020-07-27 14:00:46 -0700

    Added stuff

---
+++ os/src/task/block_driver.rs
@@ -0,0 +1,74 @@
+use conquer_once::spin::OnceCell;
+use crossbeam_queue::ArrayQueue;
+use core::{pin::Pin, task::{Poll, Context}};
+use futures_util::stream::Stream;
+use futures_util::task::AtomicWaker;
+use futures_util::stream::StreamExt;
+use pc_keyboard::{layouts, HandleControl, Keyboard, ScancodeSet1};
+use crate::println;
+use crate::keyboard_routing::KEYBOARD_ROUTER;
+use x86_64::instructions::interrupts;
+
+static WAKER: AtomicWaker = AtomicWaker::new();
+static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
+
+pub async fn print_keypresses() {
+    let mut scancodes = ScancodeStream::new();
+    let mut _keyboard = Keyboard::new(layouts::Us104Key, ScancodeSet1,
+        HandleControl::Ignore);
+
+    while let Some(scancode) = scancodes.next().await {
+            interrupts::without_interrupts(|| {
+                KEYBOARD_ROUTER.lock().handle_scancode(scancode);
+            });
+    }
+}
+
+
+pub(crate) fn add_scancode(scancode: u8) {
+    if let Ok(queue) = SCANCODE_QUEUE.try_get() {
+        if let Err(_) = queue.push(scancode) {
+            println!("WARNING: scancode queue full; dropping keyboard input");
+        }else{
+            WAKER.wake();
+        }
+    } else {
+        println!("WARNING: scancode queue uninitialized");
+    }
+}
+
+pub struct ScancodeStream {
+    _private: (),
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE.try_init_once(|| ArrayQueue::new(100))
+            .expect("ScancodeStream::new should only be called once");
+        ScancodeStream { _private: () }
+    }
+}
+
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE
+            .try_get()
+            .expect("scancode queue not initialized");
+
+        // fast path
+        if let Ok(scancode) = queue.pop() {
+            return Poll::Ready(Some(scancode));
+        }
+
+        WAKER.register(&cx.waker());
+        match queue.pop() {
+            Ok(scancode) => {
+                WAKER.take();
+                Poll::Ready(Some(scancode))
+            }
+            Err(crossbeam_queue::PopError) => Poll::Pending,
+        }
+    }
+}
