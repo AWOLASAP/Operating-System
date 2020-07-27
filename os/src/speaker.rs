d97c1c7f8c9bff2f79ab4ae4a61e383057b28349
Griffin Walraven <griffin.walraven@gmail.com>
2020-07-27 10:37:15 -0700

    empyt args defaults to 0, a freq of 0 ends the play_sound func as well

--- os/src/speaker.rs
+++ os/src/speaker.rs
@@ -9,10 +9,12 @@ pub fn play_sound(frequence: i32) {
 
     // Set the PIT to the desired frequency
     if frequence == 0 {
-        prinln!("Invalid Frequency: {}", frequence);
+        println!("\nInvalid Frequency: {}", frequence);
+        return;
     } else {
         div = 1193180 / frequence;
     }
+
     unsafe {
         outb(0x43, 0xb6);
         outb(0x42, (div) as u8);
@@ -25,6 +27,8 @@ pub fn play_sound(frequence: i32) {
         unsafe { outb(0x61, tmp as u8| 3); }
         println!("{}", tmp as u8 | 3)
     }
+
+    println!("\nBEEP!");
 }
 
 // Make it shutup
@@ -38,7 +42,6 @@ pub fn no_sound() {
 pub fn beep(freq: i32) {
     play_sound(freq);
     //timer_wait(10);
-    println!("\nBEEP!");
     no_sound();
     //set_PIT_2(old_frequency);
 }
