6e370d691862aac9f97a1fddcee5cf7a5e3cfdd0
Otis Root <otis@otisroot.com>
2020-07-29 13:59:42 -0700

    So biw have the front end API defined

--- os/src/ustar.rs
+++ os/src/ustar.rs
@@ -4,6 +4,7 @@ use os::ata_block_driver::AtaPio;
 use alloc::vec::Vec;
 use alloc::string::String;
 use hashbrown::HashMap;
+use core::option::Option;
 
 trait USTARItem {
     fn get_name(&self) -> String;
@@ -62,6 +63,38 @@ impl UstarFileSystem {
     // Directory based seeking functions (will handle things like ls in the future)
     pub fn get_id(&mut self) {
         self.current_dirs.insert(self.current_dirs_tracker, root);
+    }
+
+    pub fn list_files(&self, id: u64) -> Vec<String> {
+
+    }
+
+    pub fn list_subdirectories(&self, id: u64) -> Vec<String> {
+
+    }
+
+    pub fn change_directory(&mut self, directory: String, id: u64) -> bool {
+
+    }
+
+    pub fn change_directory_absolute_path(&mut self, path: String, id: u64) -> bool {
+
+    }
+
+    pub fn read_file(&self, file: String, id: u64) -> Option<Vec<u8>> {
+
+    }
+
+    pub fn read_file_absolute_path(&self, path: String, id: u64) -> Option<Vec<u8>> {
+        
+    }
+
+    pub fn write_file(&self, file: String, data: Vec<u8>, id: u64) -> bool {
+
+    }
+
+    pub fn write_file_absolute_path(&self, path: String, data: Vec<u8>, id: u64) -> bool {
+        
     }
 }
 
